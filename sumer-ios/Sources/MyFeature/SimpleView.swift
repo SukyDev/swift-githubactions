c978b8e4fc10e40bfa0a252f3c42d3ac417161d4
Luís Machado <luismachado@Luiss-MacBook-Pro-2.local>
2024-11-13 15:56:16 +0000

    Created simple view

---
+++ sumer-ios/Sources/MyFeature/SimpleView.swift
@@ -0,0 +1,25 @@
+//
+//  SimpleView.swift
+//  sumer-ios
+//
+//  Created by Luís Machado on 13/11/2024.
+//
+
+import SwiftUI
+
+public struct SimpleView: View {
+
+    public init() {}
+
+    public var body: some View {
+        VStack {
+            Text("My Simple View")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SimpleView()
+}
+
---
+++ sumer-ios/Tests/MyFeatureTests/SimpleViewTests.swift
@@ -0,0 +1,19 @@
+//
+//  SimpleViewTests.swift
+//  sumer-ios
+//
+//  Created by Luís Machado on 13/11/2024.
+//
+
+import Foundation
+import SnapshotTesting
+import MyTestSupport
+import XCTest
+@testable import MyFeature
+
+final class SimpleViewTests: XCTestCase {
+    func testExample() throws {
+        let view = MyFeature.SimpleView()
+        SumerSnapshotTestCase().snapshotTest(view: view)
+    }
+}
