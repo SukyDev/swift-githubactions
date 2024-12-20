fdee1eb763348b7cc2e43fb840f8370c738d53ae
Vladimir Sukanica <vladimir.sukanica@zesium.com>
2024-12-20 12:50:36 +0100

    test existing snapshots

--- sumer-ios/Package.swift
+++ sumer-ios/Package.swift
@@ -26,10 +26,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyFeature",
-            dependencies: [
-                .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
-            ]
+            name: "MyFeature"
         ),
         .target(
             name: "MyTestSupport",
