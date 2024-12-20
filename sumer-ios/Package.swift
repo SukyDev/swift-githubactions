a7b7122d0f52e2e5599aa54f2475cfc056abe130
Vladimir Sukanica <vladimir.sukanica@zesium.com>
2024-12-20 09:20:34 +0100

    Remove macOS target for testing

--- sumer-ios/Package.swift
+++ sumer-ios/Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "sumer-ios",
     platforms: [
-        .iOS(.v17),
-        .macOS(.v10_15)
+        .iOS(.v17)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
